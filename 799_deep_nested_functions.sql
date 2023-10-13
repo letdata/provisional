fcddb74bef3b311145498a92ae5219dfc89edc17
Luis Miguel <lxmiguel@netscape.net>
2023-10-13 19:42:49 -0400

    Create 799_deep_nested_functions.sql

---
+++ 799_deep_nested_functions.sql
@@ -0,0 +1,29 @@
+use utah; -- lm-ws2012-1
+go
+
+select
+	salary6 as salary
+from ( -- s6
+	select
+		cast(salary5 as decimal(10,2)) as salary6
+	from ( -- s5
+		select
+			rtrim(salary4) as salary5
+		from ( -- s4
+			select
+				replace(salary3, ',', '') as salary4
+			from ( -- s3
+				select
+					ltrim(salary2) as salary3
+				from ( -- s2
+					select
+						replace(salary1, '$', '') as salary2
+					from ( -- s1
+						select '$    65,999.00   ' as salary1 -- MOST INNER QUERY - FIRST TO RUN AND RETURN SOMETHING
+					) s1
+				) s2
+			) s3
+		) s4
+		-- 65,999
+	) s5
+) s6
